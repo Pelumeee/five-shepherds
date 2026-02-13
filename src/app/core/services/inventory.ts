99e51cac135790276e4b5de30e4512acce1c52ef
Oluwapelumi <classicpelmythe2@gmail.com>
2026-02-13 18:57:05 +0100

    finished addition of product to an inventory

--- src/app/core/services/inventory.ts
+++ src/app/core/services/inventory.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import {
   doc,
   getDoc,
@@ -10,20 +10,69 @@ import {
 } from 'firebase/firestore';
 import { Firebase } from '../../core/services/firebase';
 
-export interface Inventory {
+// export interface Inventory {
+//   sku: string;
+//   quantity: number;
+//   sellingPrice: number;
+//   costPrice?: number;
+//   unit?: 'pcs' | 'kg' | 'ltr';
+//   minStock?: number;
+//   createdAt?: any;
+//   updatedAt?: any;
+// }
+
+export interface InventoryPayload {
   sku: string;
   quantity: number;
+  unit: 'pcs' | 'kg' | 'ltr';
+  minStock?: number;
   sellingPrice: number;
   costPrice?: number;
-  unit?: 'pcs' | 'kg' | 'ltr';
-  minStock?: number;
-  createdAt?: any;
-  updatedAt?: any;
+  barCode?: string;
+  imageUrl: string;
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class Inventory {
-  
+export class InventoryService {
+  firebase = inject(Firebase);
+
+  async createInventory(payload: InventoryPayload): Promise<string> {
+    const ref = doc(this.firebase.firestore, 'inventory', payload.sku);
+
+    await setDoc(ref, {
+      ...payload,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+    });
+
+    return ref.id;
+  }
+
+  /* ================= GET ONE ================= */
+  async getInventoryBySku(sku: string): Promise<InventoryPayload | null> {
+    const ref = doc(this.firebase.firestore, 'inventory', sku);
+    const snap = await getDoc(ref);
+
+    return snap.exists() ? (snap.data() as InventoryPayload) : null;
+  }
+
+  /* ================= GET ALL ================= */
+  async getAllInventory(): Promise<InventoryPayload[]> {
+    const ref = collection(this.firebase.firestore, 'inventory');
+    const snap = await getDocs(ref);
+
+    return snap.docs.map((doc) => doc.data() as InventoryPayload);
+  }
+
+  /* ================= UPDATE ================= */
+  async updateInventory(sku: string, updates: Partial<InventoryPayload>): Promise<void> {
+    const ref = doc(this.firebase.firestore, 'inventory', sku);
+
+    await updateDoc(ref, {
+      ...updates,
+      updatedAt: serverTimestamp(),
+    });
+  }
 }
--- src/app/pages/inventory/new/new.ts
+++ src/app/pages/inventory/new/new.ts
@@ -1,20 +1,34 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { TitleCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { Notification } from '../../settings-layout/preferences/components/notification/notification';
 import { ProductService } from '../../../core/services/product';
+import { ToastService } from '../../../core/services/toast';
+import { InventoryPayload, InventoryService } from '../../../core/services/inventory';
 
 @Component({
   selector: 'app-new',
-  imports: [Notification, RouterLink, TitleCasePipe],
+  imports: [Notification, RouterLink, TitleCasePipe, FormsModule],
   templateUrl: './new.html',
 })
 export class New {
   private route = inject(ActivatedRoute);
   private productService = inject(ProductService);
+  private inventoryService = inject(InventoryService);
+  toast = inject(ToastService);
 
   selectedProduct = signal<Product | null>(null);
+
+  sku = signal('');
+  unit = signal<InventoryPayload['unit'] | null>(null);
+  imageUrl = signal('');
+  quantity = 1;
+  sellingPrice = 0.0;
+  costPrice = 0.0;
+  barCode = '';
+  minStock = 1;
 
   async ngOnInit() {
     const sku = this.route.snapshot.queryParamMap.get('sku');
@@ -22,8 +36,30 @@ export class New {
     if (sku) {
       const product = await this.productService.getProductBySku(sku);
       this.selectedProduct.set(product);
-      console.log(product);
+      this.sku.set(product!.sku);
+      this.unit.set(product!.unit);
+      this.imageUrl.set(product!.imageUrl!);
     }
+  }
+
+  async addProdInventory() {
+    if (!this.sellingPrice || !this.quantity) {
+      this.toast.show('Please fill required inputs', 'error');
+      return;
+    }
+
+    const payLoad: InventoryPayload = {
+      sku: this.sku(),
+      quantity: this.quantity,
+      unit: this.unit()!,
+      minStock: this.minStock,
+      sellingPrice: this.sellingPrice,
+      costPrice: this.costPrice,
+      barCode: this.barCode,
+      imageUrl: this.imageUrl(),
+    };
+    const savedInventory = await this.inventoryService.createInventory(payLoad);
+    console.log(savedInventory);
   }
 
   testObj = {
