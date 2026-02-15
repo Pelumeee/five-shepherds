235718f293b6058c6e836a15e4da5fdf9beea5e6
Oluwapelumi <classicpelmythe2@gmail.com>
2026-02-15 21:48:34 +0100

    created a inventory card component

---
+++ src/app/pages/inventory/components/inventory-card/inventory-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-card',
+  imports: [],
+  templateUrl: './inventory-card.html',
+  styleUrl: './inventory-card.css',
+})
+export class InventoryCard {
+
+}
