b2e02baa6280b14b79c0a65575417a7d33ec1453
Oluwapelumi <classicpelmythe2@gmail.com>
2026-02-24 11:53:58 +0100

    setting up Order services

--- src/app/core/services/order.ts
+++ src/app/core/services/order.ts
@@ -11,73 +11,36 @@ import {
 } from 'firebase/firestore';
 import { Firebase } from '../../core/services/firebase';
 
-export interface InventoryPayload {
+export interface OrderService {
+  id?: string;
+  orderNumber: string;
+
+  inventoryId: string;
+
   productName: string;
   sku: string;
-  quantity: number;
-  unit: 'pcs' | 'kg' | 'ltr';
-  minStock?: number;
-  sellingPrice: number;
-  costPrice?: number;
-  barCode?: string;
-  imageUrl: string;
-  productCategory: string;
-}
+  productImage: string;
 
-export interface InventoryObject extends InventoryPayload {
-  createdAt?: any;
-  updatedAt?: any;
+  price: number;
+  quantity: number;
+  totalPrice: number;
+
+  offerPrice?: number;
+  counterPrice?: number;
+
+  status: 'pending' | 'accepted' | 'rejected' | 'countered';
+
+  customerName?: string;
+
+  createdBy: 'external' | 'admin';
+
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class InventoryService {
   firebase = inject(Firebase);
-
-  async createInventory(payload: InventoryPayload): Promise<string> {
-    const ref = doc(this.firebase.firestore, 'inventory', payload.sku);
-    const snap = await getDoc(ref);
-
-    if (snap.exists()) {
-      throw new Error('Inventory already exists for this product');
-    }
-
-    await setDoc(ref, {
-      ...payload,
-      createdAt: serverTimestamp(),
-      updatedAt: serverTimestamp(),
-    });
-
-    return ref.id;
-  }
-
-  async getInventoryBySku(sku: string): Promise<InventoryPayload | null> {
-    const ref = doc(this.firebase.firestore, 'inventory', sku);
-    const snap = await getDoc(ref);
-
-    return snap.exists() ? (snap.data() as InventoryPayload) : null;
-  }
-
-  async getAllInventory(): Promise<InventoryObject[]> {
-    const ref = collection(this.firebase.firestore, 'inventory');
-    const snap = await getDocs(ref);
-
-    return snap.docs.map((doc) => doc.data() as InventoryObject);
-  }
-
-  async updateInventory(sku: string, updates: Partial<InventoryPayload>): Promise<void> {
-    const ref = doc(this.firebase.firestore, 'inventory', sku);
-
-    await updateDoc(ref, {
-      ...updates,
-      updatedAt: serverTimestamp(),
-    });
-  }
-
-  async deleteInventory(sku: string): Promise<void> {
-    const ref = doc(this.firebase.firestore, 'inventory', sku);
-    await deleteDoc(ref);
-  }
 }
